796e299f810a412d9df470b8bbeb99ca3ffc73a6
LennyDennis <lennydennis@gmail.com>
2020-03-30 23:34:50 +0300

    - Create home, countries, covid19 and news fragment. - Create bottom navigation to navigate between the fragments.

---
+++ app/src/main/java/com/example/covid_19/ui/CountriesFragment.java
@@ -0,0 +1,28 @@
+package com.example.covid_19.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.covid_19.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CountriesFragment extends Fragment {
+
+    public CountriesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_countires, container, false);
+    }
+}
